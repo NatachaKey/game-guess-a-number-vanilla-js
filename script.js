23364a3843a4212c6270933aef463491d9f7784a
Glitch (glitch-hello-website) <none>
2022-08-30 14:13:34 +0000

    ✈️🏬 Checkpoint ./script.js:42900803/1571 ./index.html:42900803/78

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btn= document.querySelector("#btn");
 const inputField=document.querySelector("#inputField");
 const answer= Math.floor(Math.random()*13)+1;
 
+
 btn.addEventListener("click", play);
 
 function play(){
@@ -41,7 +42,7 @@ function play(){
     else {
         Swal.fire({
             text: 'Siiiiiii! ¡Has ganado!',
-            imageUrl: 'https://images.unsplash.com/photo-1621581314625-ec70e46c76e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+            imageUrl: 'https://images.unsplash.com/photo-1631397832307-d8344ff719f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
             imageWidth: 300,
             imageHeight: 400,
             imageAlt: 'Victoria',
